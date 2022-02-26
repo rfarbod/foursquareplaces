4a5b54d116c41963bb0eb294a909149cbd5d3218
Farbod Rahiminik <farbodrahiminik@Farbods-MacBook-Pro.local>
2022-02-26 22:32:41 +0330

    first commit + added swiftUI flux for redux implementation

---
+++ FoursquarePlaces/FoursquarePlaces/FoursquarePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoursquarePlacesApp.swift
+//  FoursquarePlaces
+//
+//  Created by Farbod Rahiminik on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FoursquarePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
